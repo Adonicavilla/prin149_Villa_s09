07eb6bbaffef3cb7718fa7e3a7a47665d9457c35
Adonicavilla <villa2232782@mls.ceu.edu.ph>
2023-04-19 10:49:45 +0800

    Bootstrap Introduction

---
+++ discussion/index.js
@@ -0,0 +1,10 @@
+// Bootstrap 
+// 	- is one of the most popular CSS framework
+// 		it was developed by mark otto and jaacob thronto at twitter. It was release as an open source in August 2011
+// Feature Of Bootsrap
+// 	- easy to setup
+// 		- anyone with knowledge of html  and CSS can quickly start using Bootsrtp
+// 		- italso has good dicumentation on how to use its features
+// 	- mobile fist approach
+// 		- it consists of mobile first styling throughout its code  that helps developers create to website that are friendly to mobile screens (tablet ad mobile smartphes)
+// 
